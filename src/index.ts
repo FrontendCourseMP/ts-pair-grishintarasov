601e073ca5ffcff325ab7000c7436ef3510a5544
TarasovFedor <fedor.tarasov.2007@mail.ru>
2025-11-17 14:56:30 +0300

    tarasov second task script

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-function checkValidity(value: number, min: number, max: number) {
+function checkValidity(value: number, min: number, max: number): boolean {
     return (value >= min) && (value <= max) 
 }
 
-function calculateTimeDelay(arrival: number, delay: number) {
+function calculateTimeDelay(arrival: number, delay: number): number {
     let result: number = arrival + delay;
     
     while (result > 23) {
@@ -12,17 +12,55 @@ function calculateTimeDelay(arrival: number, delay: number) {
     return result
 }
 
+class BracketsString {
+    str: string;
+
+    static brackets = {
+        '(': ')',
+        '[': ']',
+        '{': '}',
+    }
+
+    constructor(str: string) {
+        this.str = str;
+    }
+
+    checkBrackets(): boolean {
+        let opens_stack = [];
+        let has_brackets = false;
+
+        for (const char of this.str) {
+            if (char in BracketsString.brackets) {
+                opens_stack.push(char);
+                has_brackets = true;
+            }
+            else if (Object.values(BracketsString.brackets).includes(char)) {
+                const open = opens_stack.pop();
+                has_brackets = true;
+                if (BracketsString.brackets[open] !== char) {
+                    return false
+                }
+            }
+        }
+
+        return (has_brackets && opens_stack.length === 0)
+    }
+}
+
 
 const arrival = document.getElementsByClassName('input-arr')[0] as HTMLInputElement;
 const late = document.getElementsByClassName('input-late')[0] as HTMLInputElement;
-const output = document.getElementsByClassName('first-output')[0] as HTMLOutputElement;
-
+const timeOutput = document.getElementsByClassName('time-output')[0] as HTMLOutputElement;
 const minValue: number = 0;
 const maxValue: number = 23;
-const inputs = [arrival, late];
+const timeInputs = [arrival, late];
+
+const brackets = document.getElementsByClassName('brackets-input')[0] as HTMLInputElement;
+const bracketsOutput = document.getElementsByClassName('brackets-output')[0] as HTMLOutputElement;
+const bracketsButton = document.getElementsByClassName('check-btn')[0] as HTMLButtonElement;
 
 
-inputs.forEach(_ => {
+timeInputs.forEach(_ => {
     _.addEventListener('input', () => {
         const arrivalTime: number = Number(arrival.value);
         const lateTime: number = Number(late.value);
@@ -32,6 +70,16 @@ inputs.forEach(_ => {
             answ = calculateTimeDelay(arrivalTime, lateTime);
         }
 
-        output.textContent = answ > -1 ? String(answ) : 'Некорректный ввод';
+        timeOutput.textContent = answ > -1 ? String(answ) : 'Некорректный ввод';
     });
+});
+
+bracketsButton.addEventListener('click', (event) => {
+    event.preventDefault;
+    const str: BracketsString = new BracketsString(brackets.value);
+
+    console.log(str)
+
+    bracketsOutput.textContent = str.checkBrackets() ? 
+    'Скобки в строке валидны' : 'Строка не прошла валидацию';
 });
